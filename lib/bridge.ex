678d0fd8051adc590506e5a85b8b546c8f575196
Derrick Reimer <derrickreimer@gmail.com>
2017-05-01 09:12:33 -0500

    Install credo for linting

--- lib/bridge.ex
+++ lib/bridge.ex
@@ -1,4 +1,6 @@
 defmodule Bridge do
+  @moduledoc false
+
   use Application
 
   # See http://elixir-lang.org/docs/stable/elixir/Application.html
--- mix.exs
+++ mix.exs
@@ -37,7 +37,8 @@ defmodule Bridge.Mixfile do
      {:phoenix_html, "~> 2.6"},
      {:phoenix_live_reload, "~> 1.0", only: :dev},
      {:gettext, "~> 0.11"},
-     {:cowboy, "~> 1.0"}]
+     {:cowboy, "~> 1.0"},
+     {:credo, "~> 0.7", only: [:dev, :test]}]
   end
 
   # Aliases are shortcuts or tasks specific to the current project.
