e852ab3ed2f15e61843144b9b7e1c36840da1881
Derrick Reimer <derrickreimer@gmail.com>
2017-05-05 14:57:21 -0500

    Prepare for Heroku deployment
    Following the steps outlined here:
    http://www.phoenixframework.org/docs/heroku

--- config/prod.exs
+++ config/prod.exs
@@ -14,10 +14,18 @@ use Mix.Config
 config :bridge, Bridge.Endpoint,
   http: [port: {:system, "PORT"}],
   url: [host: "example.com", port: 80],
-  cache_static_manifest: "priv/static/manifest.json"
+  cache_static_manifest: "priv/static/manifest.json",
+  secret_key_base: System.get_env("SECRET_KEY_BASE")
 
 # Do not print debug messages in production
 config :logger, level: :info
+
+# Configure your database
+config :bridge, Bridge.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  url: System.get_env("DATABASE_URL"),
+  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
+  ssl: true
 
 # ## SSL Support
 #
@@ -55,7 +63,3 @@ config :logger, level: :info
 #
 #     config :bridge, Bridge.Endpoint, server: true
 #
-
-# Finally import the config/prod.secret.exs
-# which should be versioned separately.
-import_config "prod.secret.exs"
