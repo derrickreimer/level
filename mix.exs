52f06561b77539246b87299cbdecead0d5105197
Derrick Reimer <derrickreimer@gmail.com>
2017-06-10 00:01:18 -0700

    Upgrade to phoenix 1.3.0-rc2

--- mix.exs
+++ mix.exs
@@ -30,7 +30,7 @@ defmodule Bridge.Mixfile do
   #
   # Type `mix help deps` for examples and options.
   defp deps do
-    [{:phoenix, "~> 1.2.1"},
+    [{:phoenix, "~> 1.3.0-rc"},
      {:phoenix_pubsub, "~> 1.0"},
      {:phoenix_ecto, "~> 3.0"},
      {:postgrex, ">= 0.0.0"},
