42ea29e2e85f781e10ed3184b06fe0095900e101
Derrick Reimer <derrickreimer@gmail.com>
2017-07-01 01:04:40 -0500

    Implement first pass at POSTing to signup endpoint
    - Serialize form data and post it to the server
    - Pass the CSRF token via Flags into the program
    - Parse validation errors and store them on the model

--- assets/elm/src/Signup.elm
+++ assets/elm/src/Signup.elm
@@ -2,16 +2,20 @@ module Signup exposing (..)
 
 import Html exposing (..)
 import Html.Attributes exposing (..)
-import Html.Events exposing (onInput)
+import Html.Events exposing (onInput, onClick)
 import Regex exposing (regex)
+import Http
+import Json.Encode as Encode
+import Json.Decode as Decode exposing (decodeString)
 
 
-main : Program Never Model Msg
+main : Program Flags Model Msg
 main =
-    Html.beginnerProgram
-        { model = model
+    Html.programWithFlags
+        { init = init
         , view = view
         , update = update
+        , subscriptions = subscriptions
         }
 
 
@@ -20,17 +24,36 @@ main =
 
 
 type alias Model =
-    { team_name : String
+    { csrf_token : String
+    , team_name : String
     , slug : String
     , username : String
     , email : String
     , password : String
+    , errors : List ValidationError
     }
 
 
-model : Model
-model =
-    Model "" "" "" "" ""
+type alias Flags =
+    { csrf_token : String
+    }
+
+
+init : Flags -> ( Model, Cmd Msg )
+init flags =
+    ( (initialState flags.csrf_token), Cmd.none )
+
+
+initialState : String -> Model
+initialState csrfToken =
+    { csrf_token = csrfToken
+    , team_name = ""
+    , slug = ""
+    , username = ""
+    , email = ""
+    , password = ""
+    , errors = []
+    }
 
 
 
@@ -43,25 +66,44 @@ type Msg
     | Username String
     | Email String
     | Password String
+    | Submit
+    | Submitted (Result Http.Error String)
 
 
-update : Msg -> Model -> Model
+update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
         TeamName val ->
-            { model | team_name = val, slug = (slugify val) }
+            ( { model | team_name = val, slug = (slugify val) }, Cmd.none )
 
         Slug val ->
-            { model | slug = val }
+            ( { model | slug = val }, Cmd.none )
 
         Username val ->
-            { model | username = val }
+            ( { model | username = val }, Cmd.none )
 
         Email val ->
-            { model | email = val }
+            ( { model | email = val }, Cmd.none )
 
         Password val ->
-            { model | password = val }
+            ( { model | password = val }, Cmd.none )
+
+        Submit ->
+            ( model, submit model )
+
+        Submitted (Ok teamName) ->
+            ( model, Cmd.none )
+
+        Submitted (Err (Http.BadStatus resp)) ->
+            case decodeString errorDecoder resp.body of
+                Ok value ->
+                    ( { model | errors = value }, Cmd.none )
+
+                Err _ ->
+                    ( model, Cmd.none )
+
+        Submitted (Err _) ->
+            ( model, Cmd.none )
 
 
 slugify : String -> String
@@ -71,6 +113,15 @@ slugify teamName =
         |> (Regex.replace Regex.All (regex "[^a-z0-9]+") (\_ -> "-"))
         |> (Regex.replace Regex.All (regex "(^-|-$)") (\_ -> ""))
         |> String.slice 0 20
+
+
+
+-- SUBSCRIPTIONS
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.none
 
 
 
@@ -87,14 +138,28 @@ view model =
                 [ label [ for "slug", class "form-label" ] [ text "URL" ]
                 , div [ class "slug-field" ]
                     [ div [ class "slug-field__domain" ] [ text "bridge.chat/" ]
-                    , input [ id "slug", type_ "text", class "text-field slug-field__slug", name "slug", value model.slug, onInput Slug ] []
+                    , input
+                        [ id "slug"
+                        , type_ "text"
+                        , class "text-field slug-field__slug"
+                        , name "slug"
+                        , value model.slug
+                        , onInput Slug
+                        ]
+                        []
                     ]
                 ]
             , textField Username (FormField "text" "username" "Username" model.username)
             , textField Email (FormField "email" "email" "Email Address" model.email)
             , textField Password (FormField "password" "password" "Password" model.password)
             , div [ class "form-controls" ]
-                [ button [ type_ "submit", class "button button--primary button--full" ] [ text "Sign up" ] ]
+                [ button
+                    [ type_ "submit"
+                    , class "button button--primary button--full"
+                    , onClick Submit
+                    ]
+                    [ text "Sign up" ]
+                ]
             ]
         , div [ class "auth-form__footer" ]
             [ p []
@@ -118,5 +183,83 @@ textField : (String -> msg) -> FormField -> Html msg
 textField msg field =
     div [ class "form-field" ]
         [ label [ for field.name, class "form-label" ] [ text field.label ]
-        , input [ id field.name, type_ field.type_, class "text-field text-field--full", name field.name, value field.value, onInput msg ] []
+        , input
+            [ id field.name
+            , type_ field.type_
+            , class "text-field text-field--full"
+            , name field.name
+            , value field.value
+            , onInput msg
+            ]
+            []
         ]
+
+
+
+-- HTTP
+
+
+type alias ErrorResponse =
+    { errors : List ValidationError }
+
+
+type alias ValidationError =
+    { attribute : String
+    , message : String
+    }
+
+
+submit : Model -> Cmd Msg
+submit model =
+    Http.send Submitted (buildRequest model)
+
+
+buildRequest : Model -> Http.Request String
+buildRequest model =
+    postWithCsrfToken model.csrf_token "/api/teams" (buildBody model) successDecoder
+
+
+postWithCsrfToken : String -> String -> Http.Body -> Decode.Decoder a -> Http.Request a
+postWithCsrfToken token url body decoder =
+    Http.request
+        { method = "POST"
+        , headers = [ Http.header "X-Csrf-Token" token ]
+        , url = url
+        , body = body
+        , expect = Http.expectJson decoder
+        , timeout = Nothing
+        , withCredentials = False
+        }
+
+
+buildBody : Model -> Http.Body
+buildBody model =
+    Http.jsonBody
+        (Encode.object
+            [ ( "signup"
+              , Encode.object
+                    [ ( "team_name", Encode.string model.team_name )
+                    , ( "slug", Encode.string model.slug )
+                    , ( "username", Encode.string model.username )
+                    , ( "email", Encode.string model.email )
+                    , ( "password", Encode.string model.password )
+                    ]
+              )
+            ]
+        )
+
+
+successDecoder : Decode.Decoder String
+successDecoder =
+    Decode.at [ "team", "name" ] Decode.string
+
+
+errorDecoder : Decode.Decoder (List ValidationError)
+errorDecoder =
+    Decode.field "errors"
+        (Decode.list
+            (Decode.map2 ValidationError
+                (Decode.field "attribute" Decode.string)
+                (Decode.field "message" Decode.string)
+            )
+        )
