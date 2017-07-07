5906318e4ac81c0bfcf9a1925d28dd6a8d78f3e2
Derrick Reimer <derrickreimer@gmail.com>
2017-07-07 10:31:10 -0500

    Fix prod config

--- config/prod.exs
+++ config/prod.exs
@@ -13,7 +13,7 @@ use Mix.Config
 # which you typically run after static files are built.
 config :bridge, Bridge.Web.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [host: {:system, "BRIDGE_HOST"}, port: 80],
+  url: [host: System.get_env("BRIDGE_HOST"), port: 80],
   cache_static_manifest: "priv/static/cache_manifest.json",
   secret_key_base: System.get_env("BRIDGE_SECRET_KEY_BASE")
 
