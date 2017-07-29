41a249e336ae1440efb2921dd4eabfa9ac90bb07
Derrick Reimer <derrickreimer@gmail.com>
2017-07-28 23:26:33 -0500

    Adjust mailer config

--- config/prod.exs
+++ config/prod.exs
@@ -34,9 +34,8 @@ config :bridge, Bridge.Mailer,
   port: {:system, "BRIDGE_SMTP_PORT"},
   username: {:system, "BRIDGE_SMTP_USERNAME"},
   password: {:system, "BRIDGE_SMTP_PASSWORD"},
-  tls: :if_available, # can be `:always` or `:never`
-  allowed_tls_versions: [:"tlsv1", :"tlsv1.1", :"tlsv1.2"], # or {":system", ALLOWED_TLS_VERSIONS"} w/ comma seprated values (e.g. "tlsv1.1,tlsv1.2")
-  ssl: true,
+  tls: :always,
+  ssl: false,
   retries: 1
 
 # ## SSL Support
