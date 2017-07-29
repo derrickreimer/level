339ee2be20396a928a2f102b8ba796396171963a
Derrick Reimer <derrickreimer@gmail.com>
2017-07-29 12:56:37 -0500

    Move scalars into their own file

---
+++ lib/bridge_web/schema/scalars.ex
@@ -0,0 +1,17 @@
+defmodule BridgeWeb.Schema.Scalars do
+  @moduledoc """
+  GraphQL scalar type definitions.
+  """
+
+  use Absinthe.Schema.Notation
+
+  @desc """
+  The `Time` scalar type represents time values provided in the ISOz
+  datetime format (that is, the ISO 8601 format without the timezone offset, e.g.,
+  "2015-06-24T04:50:34Z").
+  """
+  scalar :time, description: "ISOz time" do
+    parse &Timex.parse(&1.value, "{ISO:Extended:Z}")
+    serialize &Timex.format!(&1, "{ISO:Extended:Z}")
+  end
+end
--- lib/bridge_web/schema/types.ex
+++ lib/bridge_web/schema/types.ex
@@ -4,20 +4,10 @@ defmodule BridgeWeb.Schema.Types do
   """
 
   use Absinthe.Schema.Notation
-
   alias BridgeWeb.Schema.Helpers
 
   import_types BridgeWeb.Schema.Enums
-
-  @desc """
-  The `Time` scalar type represents time values provided in the ISOz
-  datetime format (that is, the ISO 8601 format without the timezone offset, e.g.,
-  "2015-06-24T04:50:34Z").
-  """
-  scalar :time, description: "ISOz time" do
-    parse &Timex.parse(&1.value, "{ISO:Extended:Z}")
-    serialize &Timex.format!(&1, "{ISO:Extended:Z}")
-  end
+  import_types BridgeWeb.Schema.Scalars
 
   @desc "A `User` represents a person belonging to a specific `Team`."
   object :user do
