acc5b88b38cceb44ea48b1df821cf8d41037f327
Derrick Reimer <derrickreimer@gmail.com>
2017-08-28 23:46:46 -0500

    Begin main view scaffolding

--- assets/elm/src/Main.elm
+++ assets/elm/src/Main.elm
@@ -1,7 +1,55 @@
 module Main exposing (..)
 
-import Html exposing (text, Html)
+import Html exposing (..)
+import Html.Attributes exposing (..)
 
-main : Html a
+
+main : Program Never Model Msg
 main =
-  text "Hello World"
+    Html.beginnerProgram
+        { model = model
+        , view = view
+        , update = update
+        }
+
+
+
+-- MODEL
+
+
+type alias Model =
+    { team_name : String
+    }
+
+
+model : Model
+model =
+    { team_name = "" }
+
+
+
+-- UPDATE
+
+
+type Msg
+    = Bootstrapped
+
+
+update : Msg -> Model -> Model
+update msg model =
+    case msg of
+        Bootstrapped ->
+            model
+
+
+
+-- VIEW
+
+
+view : Model -> Html Msg
+view model =
+    div [ id "app" ]
+        [ div [ class "sidebar sidebar--left" ] []
+        , div [ class "sidebar sidebar--right" ] []
+        , div [ class "main" ] []
+        ]
