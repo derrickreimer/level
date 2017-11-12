e4cdab50bac1ca4d395efdb448bee34b1147077c
Derrick Reimer <derrickreimer@gmail.com>
2017-11-11 18:21:27 -0600

    Add insertedAtTs to room messages

--- lib/level_web/schema/scalars.ex
+++ lib/level_web/schema/scalars.ex
@@ -7,13 +7,23 @@ defmodule LevelWeb.Schema.Scalars do
   alias LevelWeb.Schema.Cursor
 
   @desc """
-  The `Time` scalar type represents time values provided in the ISOz
-  datetime format (that is, the ISO 8601 format without the timezone offset, e.g.,
+  This scalar type represents time values provided in the ISOz datetime format
+  (that is, the ISO 8601 format without the timezone offset, e.g.,
   "2015-06-24T04:50:34Z").
   """
   scalar :time do
     parse &Timex.parse(&1.value, "{ISO:Extended:Z}")
     serialize &Timex.format!(&1, "{ISO:Extended:Z}")
+  end
+
+  @desc """
+  This scalar type represents time values as a Unix timestamp (in milliseconds).
+  """
+  scalar :timestamp do
+    parse &Timex.from_unix(&1.value, :millisecond)
+    serialize fn time ->
+      DateTime.to_unix(Timex.to_datetime(time), :millisecond)
+    end
   end
 
   @desc "A cursor for pagination."
