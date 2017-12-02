3b8cd90efc9cb276075c5a1862c2f35a5b71b245
Derrick Reimer <derrickreimer@gmail.com>
2017-12-02 11:19:47 -0800

    Better connections docs

--- lib/level/connections.ex
+++ lib/level/connections.ex
@@ -4,21 +4,48 @@ defmodule Level.Connections do
   """
 
   @doc """
-  Fetch users belonging to given space.
+  Fetch users belonging to a space.
+
+  ## Examples
+
+      users(space, %{limit: 2})
+      => {:ok, %Level.Pagination.Result{
+        edges: [%User{...}],
+        page_info: %{...},
+        total_count: 10
+      }}
   """
   def users(space, args, context \\ %{}) do
     Level.Connections.Users.get(space, args, context)
   end
 
   @doc """
-  Fetch drafts belonging to a given user.
+  Fetch drafts belonging to a user.
+
+  ## Examples
+
+      drafts(user, %{limit: 2})
+      => {:ok, %Level.Pagination.Result{
+        edges: [%Draft{...}],
+        page_info: %{...},
+        total_count: 10
+      }}
   """
   def drafts(user, args, context \\ %{}) do
     Level.Connections.Drafts.get(user, args, context)
   end
 
   @doc """
-  Fetch room subscriptions belonging to a given user.
+  Fetch room subscriptions belonging to a user.
+
+  ## Examples
+
+      room_subscriptions(user, %{limit: 2})
+      => {:ok, %Level.Pagination.Result{
+        edges: [%RoomSubscription{...}],
+        page_info: %{...},
+        total_count: 10
+      }}
   """
   def room_subscriptions(user, args, context \\ %{}) do
     Level.Connections.RoomSubscriptions.get(user, args, context)
@@ -26,6 +53,15 @@ defmodule Level.Connections do
 
   @doc """
   Fetch messages for a room.
+
+  ## Examples
+
+      room_messages(room, %{limit: 2})
+      => {:ok, %Level.Pagination.Result{
+        edges: [%Rooms.Message{...}],
+        page_info: %{...},
+        total_count: 10
+      }}
   """
   def room_messages(room, args, context \\ %{}) do
     Level.Connections.RoomMessages.get(room, args, context)
