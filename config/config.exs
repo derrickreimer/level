a67336bbe11f37b7ad1ebc290196640fd19457aa
Derrick Reimer <derrickreimer@gmail.com>
2017-12-21 13:35:00 -0600

    Default to .test (thx Chrome), add server script

--- config/config.exs
+++ config/config.exs
@@ -8,11 +8,11 @@ use Mix.Config
 # General application configuration
 config :level,
   ecto_repos: [Level.Repo],
-  mailer_host: System.get_env("LEVEL_MAILER_HOST") || "level.dev"
+  mailer_host: System.get_env("LEVEL_MAILER_HOST") || "level.test"
 
 # Configures the endpoint
 config :level, LevelWeb.Endpoint,
-  url: [host: System.get_env("LEVEL_HOST") || "level.dev"],
+  url: [host: System.get_env("LEVEL_HOST") || "level.test"],
   secret_key_base: "88kKPFnN/WU+4j79qm1tucW43qkoNjH0Ju54I8X2+BpKzMqYbiq4yVwXuhf7HDzr",
   render_errors: [view: LevelWeb.ErrorView, accepts: ~w(html json)],
   pubsub: [name: Level.PubSub,
---
+++ script/server
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+iex -S mix phx.server
