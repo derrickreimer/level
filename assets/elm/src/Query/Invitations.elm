42cceb718cd927ef57930c29a531b21263dfbbc9
Derrick Reimer <derrickreimer@gmail.com>
2018-01-25 10:21:11 -0500

    Add front-end logic to pull invitations

---
+++ assets/elm/src/Query/Invitations.elm
@@ -0,0 +1,74 @@
+module Query.Invitations exposing (Params, Response(..), Data, decoder, request)
+
+import Http
+import Json.Encode as Encode
+import Json.Decode as Decode
+import Json.Decode.Pipeline as Pipeline
+import Data.Invitation exposing (InvitationConnection, invitationConnectionDecoder)
+import GraphQL
+
+
+type alias Params =
+    { afterCursor : String
+    , limit : Int
+    }
+
+
+type alias Data =
+    { invitations : InvitationConnection
+    }
+
+
+type Response
+    = Found Data
+
+
+query : String
+query =
+    """
+      query GetInvitations(
+        $afterCursor: Cursor
+        $limit: Int!
+      ) {
+        viewer {
+          space {
+            invitations(first: $limit, after: $afterCursor) {
+              edges {
+                node {
+                  id
+                  email
+                  insertedAt
+                }
+              }
+              totalCount
+            }
+          }
+        }
+      }
+    """
+
+
+variables : Params -> Encode.Value
+variables params =
+    Encode.object
+        [ ( "afterCursor", Encode.string params.afterCursor )
+        , ( "limit", Encode.string params.limit )
+        ]
+
+
+foundDecoder : Decode.Decoder Response
+foundDecoder =
+    Decode.map Found
+        (Pipeline.decode Data
+            |> Pipeline.custom (Decode.at [ "invitations" ] invitationConnectionDecoder)
+        )
+
+
+decoder : Decode.Decoder Response
+decoder =
+    Decode.at [ "data", "viewer", "space" ] foundDecoder
+
+
+request : String -> Params -> Http.Request Response
+request apiToken params =
+    GraphQL.request apiToken query (Just (variables params)) decoder
