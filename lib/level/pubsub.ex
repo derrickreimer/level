1e0f9047f2cfe681fafad608fe7f4099da19ce82
Derrick Reimer <derrickreimer@gmail.com>
2018-02-10 14:02:57 -0600

    Document pubsub module

--- lib/level/pubsub.ex
+++ lib/level/pubsub.ex
@@ -1,4 +1,9 @@
 defmodule Level.Pubsub do
+  @moduledoc """
+  This module encapsulates behavior for publishing messages to listeners
+  (such as Absinthe GraphQL subscriptions).
+  """
+
   def publish(payload, topics) do
     Absinthe.Subscription.publish(LevelWeb.Endpoint, payload, topics)
   end
