7ff4efff7e3c1648a729c03c1d000746b834c9d2
Derrick Reimer <derrickreimer@gmail.com>
2018-04-13 16:45:35 -0500

    Create posts table

---
+++ priv/repo/migrations/20180413214033_create_posts.exs
@@ -0,0 +1,24 @@
+defmodule Level.Repo.Migrations.CreatePosts do
+  use Ecto.Migration
+
+  def up do
+    execute("CREATE TYPE post_state AS ENUM ('OPEN','CLOSED')")
+
+    create table(:posts, primary_key: false) do
+      add :id, :binary_id, primary_key: true
+      add :space_id, references(:spaces, on_delete: :nothing, type: :binary_id), null: false
+      add :user_id, references(:users, on_delete: :nothing, type: :binary_id), null: false
+      add :state, :post_state, null: false, default: "OPEN"
+      add :body, :text, null: false
+
+      timestamps()
+    end
+
+    create(index(:posts, [:id]))
+  end
+
+  def down do
+    drop(table(:posts))
+    execute("DROP TYPE post_state")
+  end
+end
