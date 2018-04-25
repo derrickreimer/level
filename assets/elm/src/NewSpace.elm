86bd679dee8def4a5c7e36b5ba99399891176b65
Derrick Reimer <derrickreimer@gmail.com>
2018-04-25 15:11:17 -0700

    Style the slug field

--- assets/elm/src/NewSpace.elm
+++ assets/elm/src/NewSpace.elm
@@ -127,39 +127,47 @@ type alias FormField =
 
 view : Model -> Html Msg
 view model =
-    div [ class "container mx-auto px-4 py-24 flex justify-center max-w-430px" ]
-        [ div [ class "w-full" ]
-            [ div [ class "mb-8 text-center" ]
-                [ img [ src "/images/logo-md.svg", class "logo-md", alt "Level" ] []
-                ]
-            , div
-                [ classList
-                    [ ( "px-16 py-8 bg-white rounded-lg border", True )
-                    , ( "shake", not (List.isEmpty model.errors) )
+    let
+        slugErrors =
+            errorsFor "slug" model.errors
+
+        slugClasses =
+            [ ( "input-field inline-flex", True )
+            , ( "input-field-error", not (List.isEmpty slugErrors) )
+            ]
+    in
+        div [ class "container mx-auto px-4 py-24 flex justify-center max-w-430px" ]
+            [ div [ class "w-full" ]
+                [ div [ class "mb-8 text-center" ]
+                    [ img [ src "/images/logo-md.svg", class "logo-md", alt "Level" ] []
                     ]
-                ]
-                [ h1 [ class "text-center text-2xl font-extrabold text-dusty-blue-darker pb-8" ]
-                    [ text "Create a new space" ]
-                , div [ class "pb-6" ]
-                    [ label [ for "name", class "input-label" ] [ text "Name your space" ]
-                    , textField (FormField "text" "name" "Smith, Co." model.name NameChanged)
-                        (errorsFor "name" model.errors)
+                , div
+                    [ classList
+                        [ ( "px-16 py-8 bg-white rounded-lg border", True )
+                        , ( "shake", not (List.isEmpty model.errors) )
+                        ]
                     ]
-                , div [ class "pb-6" ]
-                    [ label [ for "slug", class "input-label" ] [ text "Pick your URL" ]
-                    , textField (FormField "text" "slug" "smith-co" model.slug SlugChanged)
-                        (errorsFor "slug" model.errors)
+                    [ h1 [ class "text-center text-2xl font-extrabold text-dusty-blue-darker pb-8" ]
+                        [ text "Create a new space" ]
+                    , div [ class "pb-6" ]
+                        [ label [ for "name", class "input-label" ] [ text "Name your space" ]
+                        , textField (FormField "text" "name" "Smith, Co." model.name NameChanged)
+                            (errorsFor "name" model.errors)
+                        ]
+                    , div [ class "pb-6" ]
+                        [ label [ for "slug", class "input-label" ] [ text "Pick your URL" ]
+                        , slugField model.slug (errorsFor "slug" model.errors)
+                        ]
+                    , button
+                        [ type_ "submit"
+                        , class "btn btn-blue w-full"
+                        , onClick Submit
+                        , disabled (model.formState == Submitting)
+                        ]
+                        [ text "Let's get started" ]
                     ]
-                , button
-                    [ type_ "submit"
-                    , class "btn btn-blue w-full"
-                    , onClick Submit
-                    , disabled (model.formState == Submitting)
-                    ]
-                    [ text "Let's get started" ]
                 ]
             ]
-        ]
 
 
 textField : FormField -> List ValidationError -> Html Msg
@@ -181,6 +189,38 @@ textField field errors =
                 , onInput field.onInput
                 ]
                 []
+            , formErrors errors
+            ]
+
+
+slugField : String -> List ValidationError -> Html Msg
+slugField slug errors =
+    let
+        classes =
+            [ ( "input-field inline-flex", True )
+            , ( "input-field-error", not (List.isEmpty errors) )
+            ]
+    in
+        div []
+            [ div [ classList classes ]
+                [ label
+                    [ for "slug"
+                    , class "flex-none text-dusty-blue-dark select-none"
+                    ]
+                    [ text "https://level.space/" ]
+                , div [ class "flex-1" ]
+                    [ input
+                        [ id "slug"
+                        , type_ "text"
+                        , class "w-full p-0 ml-1 no-outline text-dusty-blue-darker"
+                        , name "slug"
+                        , placeholder "smith-co"
+                        , value slug
+                        , onInput SlugChanged
+                        ]
+                        []
+                    ]
+                ]
             , formErrors errors
             ]
 
