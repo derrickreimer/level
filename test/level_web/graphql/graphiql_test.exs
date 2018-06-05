7efda8d97e7ec5322b5ccc9c49d5906cf2ec99ac
João Pedro Evangelista <evangelistajoaop@gmail.com>
2018-06-05 19:19:04 -0300

    Fix formatting

--- test/level_web/graphql/graphiql_test.exs
+++ test/level_web/graphql/graphiql_test.exs
@@ -1,29 +1,32 @@
 defmodule LevelWeb.GraphQL.GraphiQL do
- use LevelWeb.ConnCase, async: true
+  use LevelWeb.ConnCase, async: true
 
- describe "/grapiql authentication rules" do
+  describe "/grapiql authentication rules" do
+    setup %{conn: conn} do
+      conn =
+        conn
+        |> bypass_through(LevelWeb.Router, :anonymous_browser)
+        |> get("/graphiql")
 
-  setup %{conn: conn} do
-    conn =
-      conn
-      |> bypass_through(LevelWeb.Router, :anonymous_browser)
-      |> get("/graphiql")
+      {:ok, %{conn: conn}}
+    end
 
-    {:ok, %{conn: conn}}
+    test "unauthenticated user should not be able to access /graphiql", %{conn: conn} do
+      conn = get(conn, "/graphiql")
+      assert redirected_to(conn, 302) =~ "/login"
+    end
+
+    test "authenticated users can access /graphiql", %{conn: conn} do
+      {:ok, user} = create_user()
+
+      conn =
+        conn
+        |> sign_in(user)
+        # we want the page not the api
+        |> put_req_header("accept", "text/html")
+        |> get("/graphiql")
+
+      assert html_response(conn, 200)
+    end
   end
-  test "unauthenticated user should not be able to access /graphiql", %{conn: conn} do
-    conn = get(conn, "/graphiql")
-    assert redirected_to(conn, 302) =~ "/login"
-  end
-
-  test "authenticated users can access /graphiql", %{conn: conn} do
-    {:ok, user} = create_user()
-    conn =
-      conn
-      |> sign_in(user)
-      |> put_req_header("accept", "text/html") # we want the page not the api
-      |> get("/graphiql")
-    assert html_response(conn, 200)
-  end
- end
 end
