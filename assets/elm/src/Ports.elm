bfb413da63e461c7ffce4b08e613e647f7a05d5e
Derrick Reimer <derrickreimer@gmail.com>
2018-07-18 16:51:09 -0500

    Clean up unused ports

--- assets/elm/src/Ports.elm
+++ assets/elm/src/Ports.elm
@@ -9,23 +9,6 @@ import Socket.Types
 -- INBOUND
 
 
-type alias ScrollPosition =
-    { containerId : String
-    , fromTop : Int
-    , fromBottom : Int
-    , anchorOffset : Maybe Int
-    }
-
-
-scrollPositionDecoder : Decode.Decoder ScrollPosition
-scrollPositionDecoder =
-    Decode.map4 ScrollPosition
-        (Decode.field "containerId" Decode.string)
-        (Decode.field "fromTop" Decode.int)
-        (Decode.field "fromBottom" Decode.int)
-        (Decode.field "anchorOffset" (Decode.maybe Decode.int))
-
-
 port socketAbort : (Decode.Value -> msg) -> Sub msg
 
 
@@ -48,19 +31,6 @@ port scrollPositionReceived : (Decode.Value -> msg) -> Sub msg
 -- OUTBOUND
 
 
-type alias ScrollParams =
-    { containerId : String
-    , anchorId : String
-    , offset : Int
-    }
-
-
-type alias ScrollPositionArgs =
-    { containerId : String
-    , anchorId : Maybe String
-    }
-
-
 port sendSocket : Socket.Types.Payload -> Cmd msg
 
 
@@ -68,9 +38,6 @@ port cancelSocket : String -> Cmd msg
 
 
 port updateToken : String -> Cmd msg
-
-
-port getScrollPosition : ScrollPositionArgs -> Cmd msg
 
 
 port scrollTo : Scroll.Types.AnchorParams -> Cmd msg
