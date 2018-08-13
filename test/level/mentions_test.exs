7ab7d0974697c99ebd9672956c7aeb4712452853
Derrick Reimer <derrickreimer@gmail.com>
2018-08-13 15:45:43 -0500

    Unit test coverage

--- test/level/mentions_test.exs
+++ test/level/mentions_test.exs
@@ -2,6 +2,8 @@ defmodule Level.MentionsTest do
   use Level.DataCase, async: true
 
   alias Level.Mentions
+  alias Level.Mentions.UserMention
+  alias Level.Posts.Post
   alias Level.Repo
 
   describe "grouped_base_query/1" do
@@ -22,6 +24,32 @@ defmodule Level.MentionsTest do
       [mentioner] = mention.mentioner_ids
       assert mention.post_id == post.id
       assert Ecto.UUID.load(mentioner) == {:ok, space_user.id}
+    end
+  end
+
+  describe "dismiss_all/2" do
+    setup do
+      create_user_and_space()
+    end
+
+    test "dismisses all mentions for a given post", %{space_user: space_user, space: space} do
+      {:ok, %{group: group}} = create_group(space_user)
+      {:ok, %{space_user: another_user}} = create_space_member(space, %{handle: "tiff"})
+
+      {:ok, %{post: %Post{id: post_id} = post}} =
+        create_post(space_user, group, %{body: "Hey @tiff"})
+
+      assert %UserMention{post_id: ^post_id} =
+               another_user
+               |> Mentions.base_query()
+               |> Repo.get_by(post_id: post_id)
+
+      :ok = Mentions.dismiss_all(another_user, post)
+
+      assert nil ==
+               another_user
+               |> Mentions.base_query()
+               |> Repo.get_by(post_id: post_id)
     end
   end
 end
