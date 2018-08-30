0c6240f360930d912dfae3a1e6d26aad6d67b765
Derrick Reimer <derrickreimer@gmail.com>
2018-08-30 09:26:02 -0500

    Enable gzip

--- lib/level_web/endpoint.ex
+++ lib/level_web/endpoint.ex
@@ -13,7 +13,7 @@ defmodule LevelWeb.Endpoint do
   plug Plug.Static,
     at: "/",
     from: :level,
-    gzip: false,
+    gzip: true,
     only: ~w(css fonts images js favicon.ico robots.txt)
 
   # Code reloading can be explicitly enabled under the
