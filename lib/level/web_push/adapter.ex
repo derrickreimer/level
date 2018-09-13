186878a128521074923edd7171eda2f1b181b4f4
Derrick Reimer <derrickreimer@gmail.com>
2018-09-13 11:46:07 -0500

    Formatter

--- lib/level/web_push/adapter.ex
+++ lib/level/web_push/adapter.ex
@@ -5,5 +5,6 @@ defmodule Level.WebPush.Adapter do
 
   alias Level.WebPush.Subscription
 
-  @callback send_web_push(String.t(), Subscription.t()) :: {:ok, any()} | {:error, atom()} | no_return()
+  @callback send_web_push(String.t(), Subscription.t()) ::
+              {:ok, any()} | {:error, atom()} | no_return()
 end
