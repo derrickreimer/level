f0ec09e2c1288b8df155ffe8be03483ca524ea84
Derrick Reimer <derrickreimer@gmail.com>
2018-10-19 12:42:05 -0500

    Add loading indicator to blank screen

--- assets/elm/src/Program/Main.elm
+++ assets/elm/src/Program/Main.elm
@@ -959,7 +959,9 @@ pageView repo page pushStatus =
                 |> Html.map SearchMsg
 
         Blank ->
-            text ""
+            div [ class "flex items-center justify-center h-screen w-full bg-turquoise" ]
+                [ h1 [ class "text-3xl text-white font-black" ] [ text "Loading..." ]
+                ]
 
         NotFound ->
             text "404"
