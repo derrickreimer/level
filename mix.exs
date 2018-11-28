36820848a288430d14dd660a3a00f8677ea2788d
Derrick Reimer <derrickreimer@gmail.com>
2018-11-28 16:11:48 -0600

    Upgrade premailex to suppress media query warnings

--- mix.exs
+++ mix.exs
@@ -75,7 +75,7 @@ defmodule Level.Mixfile do
       {:mox, "~> 0.4.0", only: :test},
       {:honeybadger, "~> 0.1"},
       {:quantum, "~> 2.3"},
-      {:premailex, "~> 0.3.0"},
+      {:premailex, "~> 0.3.3"},
 
       # Amazon S3 dependencies
       {:ex_aws, "~> 2.0"},
