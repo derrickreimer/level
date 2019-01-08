d3372753c10fbbf79aa847dafc6f615b99730740
Derrick Reimer <derrickreimer@gmail.com>
2019-01-08 16:34:55 -0600

    Update compose button

--- assets/elm/src/Layout/SpaceDesktop.elm
+++ assets/elm/src/Layout/SpaceDesktop.elm
@@ -135,9 +135,9 @@ fullSidebar config =
             ]
         , div [ class "absolute pl-3 w-full overflow-y-auto", style "top" "110px", style "bottom" "70px" ]
             [ ul [ class "mb-6 list-reset leading-semi-loose select-none" ]
-                [ navLink config.space "Inbox" (Just <| Route.Inbox (Route.Inbox.init spaceSlug)) config.currentRoute
+                [ navLink config.space "Compose" (Just <| Route.NewPost (Route.NewPost.init spaceSlug)) config.currentRoute
+                , navLink config.space "Inbox" (Just <| Route.Inbox (Route.Inbox.init spaceSlug)) config.currentRoute
                 , navLink config.space "Feed" (Just <| Route.Posts (Route.Posts.init spaceSlug)) config.currentRoute
-                , navLink config.space "New" (Just <| Route.NewPost (Route.NewPost.init spaceSlug)) config.currentRoute
                 ]
             , viewUnless (List.isEmpty config.bookmarks) <|
                 div []
