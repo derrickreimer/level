568fa1cede22047c8da75f56fb6e1a6a58c628bb
Derrick Reimer <derrickreimer@gmail.com>
2019-03-11 13:55:43 -0500

    Adjust font size

--- assets/elm/src/ReplyView.elm
+++ assets/elm/src/ReplyView.elm
@@ -361,8 +361,7 @@ resolvedView config replyView data =
                     [ div
                         [ classList
                             [ ( "markdown pb-1 break-words", True )
-                            , ( "text-lg", bodyLength <= 6 )
-                            , ( "text-xl", bodyLength <= 3 )
+                            , ( "text-lg", bodyLength <= 3 )
                             ]
                         ]
                         [ RenderedHtml.node
