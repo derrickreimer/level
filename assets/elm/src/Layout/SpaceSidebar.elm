34e5c38f45976b0cf2bf77a577e0d88a9ea71a12
Derrick Reimer <derrickreimer@gmail.com>
2019-04-04 23:59:43 -0500

    Style tweaks

--- assets/elm/src/Layout/SpaceSidebar.elm
+++ assets/elm/src/Layout/SpaceSidebar.elm
@@ -235,7 +235,6 @@ channelLink config hasUnreads group =
                 [ ( "flex items-center w-full pl-3 pr-2 mr-2 no-underline transition-bg rounded-full", True )
                 , ( "text-dusty-blue-darker bg-white hover:bg-grey-light", not isCurrent )
                 , ( "text-dusty-blue-darkest bg-grey font-bold", isCurrent )
-                , ( "bg-gold", isCurrent && hasUnreads )
                 ]
             ]
             [ viewIf hasUnreads <|
@@ -293,7 +292,6 @@ directMessageLink config hasUnreads recipientIds =
                 [ ( "flex items-center w-full pl-3 pr-2 mr-2 no-underline transition-bg rounded-full", True )
                 , ( "text-dusty-blue-darker bg-white hover:bg-grey-light", not isCurrent )
                 , ( "text-dusty-blue-darkest bg-grey font-bold", isCurrent )
-                , ( "bg-gold", isCurrent && hasUnreads )
                 ]
             ]
             [ viewIf hasUnreads <|
