d71ac7fd8dd0585a24ca31d1e2b771f049b5149b
Derrick Reimer <derrickreimer@gmail.com>
2019-04-16 14:21:13 -0500

    Progress

--- lib/level/spaces/create_demo.ex
+++ lib/level/spaces/create_demo.ex
@@ -36,6 +36,8 @@ defmodule Level.Spaces.CreateDemo do
 
     # Create some posts
     create_retreat_post(space_users, groups)
+    create_redesign_post(space_users, groups)
+    create_watercooler_post(space_users, groups)
 
     {:ok, %{space: space, space_user: space_user}}
   end
@@ -100,5 +102,66 @@ defmodule Level.Spaces.CreateDemo do
       })
 
     Posts.create_reply_reaction(space_users["lisalatte"], post, reply, "👍")
+  end
+
+  defp create_redesign_post(space_users, groups) do
+    {:ok, %{post: post}} =
+      Posts.create_post(space_users["lisalatte"], groups["engineering"], %{
+        body: """
+        Hey engineers, I'm working on the new home page design and I will need a hand getting the split-testing JavaScript snippet put in place. Does anyone have time this week to assist?
+
+        No rush, we're planning to ship the redesign next week. Let me know what information you need from me. Thanks!
+        """
+      })
+
+    Posts.create_post_reaction(space_users["kevincortado"], post, "👍")
+
+    {:ok, %{reply: reply}} =
+      Posts.create_reply(space_users["kevincortado"], post, %{
+        body:
+          "I can help! The credentials should be in 1Password, but I'll let you know if I can't find the right ones."
+      })
+
+    Posts.create_reply_reaction(space_users["lisalatte"], post, reply, "🤜")
+  end
+
+  defp create_watercooler_post(space_users, groups) do
+    {:ok, %{post: post}} =
+      Posts.create_post(space_users["sarahsinglepour"], groups["watercooler"], %{
+        body: """
+        Question for the week: if you could choose to be any animal, which would you be and why?
+        """
+      })
+
+    {:ok, %{reply: reply}} =
+      Posts.create_reply(space_users["joegibraltar"], post, %{
+        body: "A sloth. They seem really relaxed!"
+      })
+
+    Posts.create_reply_reaction(space_users["lisalatte"], post, reply, "😂")
+
+    {:ok, %{reply: reply2}} =
+      Posts.create_reply(space_users["kevincortado"], post, %{
+        body: "Dog, because you're pretty much automatically everyone's friend."
+      })
+
+    Posts.create_reply_reaction(space_users["sarahsinglepour"], post, reply2, "🐶")
+    Posts.create_reply_reaction(space_users["joegibraltar"], post, reply2, "🐶")
+
+    {:ok, %{reply: reply3}} =
+      Posts.create_reply(space_users["lisalatte"], post, %{
+        body: "Dolphin, because I love swimming and being smart 😉"
+      })
+
+    Posts.create_reply_reaction(space_users["kevincortado"], post, reply3, "🐬")
+
+    {:ok, %{reply: reply4}} =
+      Posts.create_reply(space_users["sarahsinglepour"], post, %{
+        body: "![Hmm](https://media.giphy.com/media/xUPGcmF2iGsTGEFVL2/giphy.gif)"
+      })
+
+    Posts.create_reply_reaction(space_users["kevincortado"], post, reply4, "😆")
+    Posts.create_reply_reaction(space_users["lisalatte"], post, reply4, "😆")
+    Posts.create_reply_reaction(space_users["joegibraltar"], post, reply4, "😆")
   end
 end
